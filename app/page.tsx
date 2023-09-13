479fbf1e1f637c8509b1e0fb27dc41241d3bbd4c
Yuda Adi Pratama <github.yuda@gmail.com>
2023-09-13 19:53:53 +0700

    removing trash code

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
 "use client"
 
-import Image from 'next/image';
 import { ModeToggle } from '@/components/ModeToggle';
 import './container.css';
 import './walletconnect.css';
 import { Web3Button } from '@web3modal/react';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { NFTCard } from '@/components/Mint';
 
 
@@ -25,21 +16,8 @@ export default function Home() {
       <Web3Button />
       </div>
       <ModeToggle />
-        {/* Include any components that need access to Web3Modal here */}
         <div className='content'>
           <NFTCard />
-          {/* <Card>
-              <CardHeader>
-                  <CardTitle>Card Title</CardTitle>
-                  <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                  <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                  <p>Card Footer</p>
-              </CardFooter>
-          </Card> */}
         </div>
     </div>
   );
