2b846662aa586414d1f41ba012c42309c558a197
Yuda Adi Pratama <github.yuda@gmail.com>
2023-09-15 01:07:59 +0700

    add new style

--- src/components/Mint/NFTCard.tsx
+++ src/components/Mint/NFTCard.tsx
@@ -31,8 +31,8 @@ export const NFTCard = () => {
         <div className="flex items-center space-x-4">
         <Skeleton className="h-12 w-12 rounded-full" />
         <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[150px]" />
+        <Skeleton className="h-4 w-[100px]" />
         </div>
         </div>
       }
